90da6f195952991b2e638fafd0893c8e249f3feb
sanjeevjayasurya <sanjeev.jayasurya@zopsmart.com>
2020-11-23 02:40:10 +0530

    feat: add a ⭐️ component

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Star from './Star';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Star />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
