3f60396a4cca39fae0efad62a042cbe447e70013
sanjeevjayasurya <sanjeev.jayasurya@zopsmart.com>
2020-11-30 01:11:23 +0530

    useEffect hooks added ( for learning purposes )

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import React, { useState } from 'react'
-import colorData from './data/color-data.json'
+import React, { useState, useEffect } from 'react'
 import ColorList from './ColorList.js'
 import AddColorForm from './AddColorForm'
 
+function Phrase() {
+    const [val, set] = useState("")
+    const [phrase, setPhrase] = useState("initial phrase")
+
+    const createPhrase = () => {
+        setPhrase(val)
+        set("")
+    }
+    useEffect(() => {
+        console.log(`typing "${val}"`);
+    }, [val])
+    useEffect(() => {
+        console.log(`saved phrase: "${phrase}"`);
+    }, [phrase])
+    return (
+        <>
+            <label>Favourite phrase</label>
+            <input
+                type="text"
+                value={val}
+                placeholder={phrase}
+                onChange={e => set(e.target.value)}
+            />
+            <button onClick={createPhrase}>send</button>
+        </>
+    )
+}
+
 export default function App() {
-    const [colors, setColors] = useState(colorData)
     // Sending state down the component tree
     return (
         <>
             <AddColorForm />
             <ColorList />
+            <Phrase />
         </>
 
     )
